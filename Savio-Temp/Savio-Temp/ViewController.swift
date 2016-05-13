8d144194da5ef593e9f8c73488678e8815df420d
Prashant <prashant.chaudhari@vsplc.com>
2016-05-13 14:46:11 +0530

    My second Commit

--- Savio-Temp/Savio-Temp/ViewController.swift
+++ Savio-Temp/Savio-Temp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Git this is my first commit")
     }
 
     override func didReceiveMemoryWarning() {
